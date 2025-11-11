5c010bf57534fbc495fa296ce5b9155bcb0cc6cb
mjkelly63 <47276324+mjkelly63@users.noreply.github.com>
2025-11-11 08:43:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { WebSocketServer } from "ws";
 import url from "url";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type":"text/plain; charset=utf-8"});
+  res.writeHead(200, {"Content-Type":"text/plain; charset=utf-8"}); 
   res.end("WS relay online\n");
 });
 
